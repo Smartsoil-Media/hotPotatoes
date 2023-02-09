fd4b9eb5184195b1010f38ecc9be2687732a44fe
GunClayton <114902151+Smartsoil-Media@users.noreply.github.com>
2023-02-09 20:47:36 +0800

    fixed audio issues

--- app.js
+++ app.js
@@ -1,9 +1,49 @@
-var audio = new Audio('/images/Hotpotatoesong.mp3');
-var playButton = document.getElementById('wiggle');
+// var audio = new Audio('/images/Hotpotatoesong.mp3');
+// var playButton = document.getElementById('wiggle');
 
-audio.oncanplaythrough = function() {
-  playButton.addEventListener('click', function() {
-    audio.play();
-    console.log("Button Clicked!")
-  });
-};
+// audio.oncanplaythrough = function() {
+//   playButton.addEventListener('click', function() {
+//     audio.play();
+//     console.log("Button Clicked!")
+//   });
+// };
+
+
+// window.onload = function() {
+//     let audio = new Audio("/images/Hotpotatoesong.mp3");
+//     let playButton = document.getElementById('wiggle');
+  
+//     audio.onerror = function() {
+//       console.error("Failed to load the audio file");
+//     };
+  
+//     audio.oncanplay = function() {
+//       playButton.addEventListener('click', function() {
+//         audio.play();
+//         console.log("Button Clicked!");
+//       });
+//     };
+//   };
+
+
+
+window.onload = function() {
+    var audio = new Audio("/images/Hotpotatoesong.mp3");
+    var playButton = document.getElementById('wiggle');
+  
+    audio.onerror = function() {
+      console.error("Failed to load the audio file");
+    };
+  
+    audio.oncanplay = function() {
+      playButton.addEventListener('click', function() {
+        if (audio.paused) {
+          audio.play();
+          console.log("Playing");
+        } else {
+          audio.pause();
+          console.log("Paused");
+        }
+      });
+    };
+  };
