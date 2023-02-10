62e9e0dc54cc063b582f8fe4a152da8d17675494
GunClayton <114902151+Smartsoil-Media@users.noreply.github.com>
2023-02-10 09:46:07 +0800

    file cleaning

--- app.js
+++ app.js
@@ -1,32 +1,3 @@
-// var audio = new Audio('/images/Hotpotatoesong.mp3');
-// var playButton = document.getElementById('wiggle');
-
-// audio.oncanplaythrough = function() {
-//   playButton.addEventListener('click', function() {
-//     audio.play();
-//     console.log("Button Clicked!")
-//   });
-// };
-
-
-// window.onload = function() {
-//     let audio = new Audio("/images/Hotpotatoesong.mp3");
-//     let playButton = document.getElementById('wiggle');
-  
-//     audio.onerror = function() {
-//       console.error("Failed to load the audio file");
-//     };
-  
-//     audio.oncanplay = function() {
-//       playButton.addEventListener('click', function() {
-//         audio.play();
-//         console.log("Button Clicked!");
-//       });
-//     };
-//   };
-
-
-
 window.onload = function() {
     var audio = new Audio("/images/Hotpotatoesong.mp3");
     var playButton = document.getElementById('wiggle');
